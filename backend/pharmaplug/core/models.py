68e8a63c195b32b0be8462a2a1396bc031b81ff5
jaykayudo <jaykayudo@gmail.com>
2024-12-23 10:40:46 +0100

    (chore) create notification model

--- backend/pharmaplug/core/models.py
+++ backend/pharmaplug/core/models.py
@@ -210,6 +210,16 @@ class OTP(BaseModel):
             return False
 
 
+class Notification(BaseModel):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    read = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.title
+
+
 class Sickness(BaseModel):
     name = models.CharField(max_length=100)
     description = models.TextField(blank=True, null=True)
